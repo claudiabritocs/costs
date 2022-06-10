18b42e6b1666ba933c5652366608002ef36ec003
Claudia Brito <claudiabrito.pro@gmail.com>
2022-06-10 09:37:17 -0300

    [Fix]: Adjust routes and imports

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+
+import Container from './components/layout/Container.js'
 import Home from './components/pages/Home.js'
 import Contact from './components/pages/Contact.js'
 import Company from './components/pages/Company.js'
 import NewProject from './components/pages/NewProject.js'
 
-import Container from './components/layout/Container.js'
-
 function App() {
   return (
     <Router>
       <div>
-        <Link to="Home">Home</Link>
+        <Link to="/">Home</Link>
         <Link to="Company">Empresa</Link>
         <Link to="Contact">Contato</Link>
         <Link to="NewProject">Novo Projeto</Link>
@@ -18,7 +18,7 @@ function App() {
 
       <Container customClass="min-height">
         <Routes>
-          <Route path="*" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/company" element={<Company />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/newproject" element={<NewProject />} />
