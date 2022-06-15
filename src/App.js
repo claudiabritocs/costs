380924af4be4b6cbba2b831cf5a89fcb181ebbe7
Claudia Brito <claudiabrito.pro@gmail.com>
2022-06-15 20:31:56 -0300

    [Feat]: Add navbar and footer, add styles

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
-import Container from './components/layout/Container.js'
-import Home from './components/pages/Home.js'
-import Contact from './components/pages/Contact.js'
-import Company from './components/pages/Company.js'
-import NewProject from './components/pages/NewProject.js'
+import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Home from './components/pages/Home'
+import Projects from './components/pages/Projects'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="Company">Empresa</Link>
-        <Link to="Contact">Contato</Link>
-        <Link to="NewProject">Novo Projeto</Link>
-      </div>
+      <Navbar />
 
       <Container customClass="min-height">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
           <Route path="/company" element={<Company />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/newproject" element={<NewProject />} />
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
