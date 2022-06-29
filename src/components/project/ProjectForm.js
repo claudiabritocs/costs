00514b8c09b34ef26a061e79a46f3e0f468d93bc
Claudia Brito <claudiabrito.pro@gmail.com>
2022-06-29 18:55:52 -0300

    [Feat]: Add fake db and include useEffect in the project to fetch the API

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,30 +1,51 @@
+import { useState, useEffect } from 'react'
+
 import styles from './ProjectForm.module.css'
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitBtn from '../form/SubmitBtn'
 
 function ProjectForm({btnText}) {
-  return (
-    <form className={styles.form}>
-      <Input
-        type="text"
-        text="Nome do projeto"
-        name="name"
-        placeholder="Insira o nome do projeto"
-      />
-      <Input
-        type="number"
-        text="Orçamento do Projeto"
-        name="budget"
-        placeholder="Insira o orçamento total"
-      />
-      <Select
-        name="category_id"
-        text="Selecione a categoria"
-      />
-      <SubmitBtn text={btnText} />
-    </form>
-  )
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setCategories(data)
+            })
+            .catch((err) => console.log(err))
+    }, [])
+
+
+    return (
+        <form className={styles.form}>
+            <Input
+                type="text"
+                text="Nome do projeto"
+                name="name"
+                placeholder="Insira o nome do projeto"
+            />
+            <Input
+                type="number"
+                text="Orçamento do Projeto"
+                name="budget"
+                placeholder="Insira o orçamento total"
+            />
+            <Select
+                name="category_id"
+                text="Selecione a categoria"
+                options={categories}
+            />
+            <SubmitBtn text={btnText} />
+        </form>
+    )
 }
 
 export default ProjectForm
