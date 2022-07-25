93bfcd68ed06f633db8dae174d4d9322a55070a7
Claudia Brito <claudiabrito.pro@gmail.com>
2022-07-25 07:49:07 -0300

    [Feat]: Create edit page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Container from './components/layout/Container'
 import Navbar from './components/layout/Navbar'
@@ -7,6 +7,7 @@ import Projects from './components/pages/Projects'
 import Contact from './components/pages/Contact'
 import Company from './components/pages/Company'
 import NewProject from './components/pages/NewProject'
+import Project from './components/pages/Project'
 import Footer from './components/layout/Footer'
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
           <Route path="/company" element={<Company />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/newproject" element={<NewProject />} />
+          <Route path="/project/:id" element={<Project />} />
         </Routes>
       </Container>
       <Footer />
